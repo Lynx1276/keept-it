f67e3ae52d3dda2fb78d1d605e736f49fa85ba5e
Lynx1276 <mlbausa84@gmail.com>
2025-02-17 18:59:00 +0800

    add another function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center bg-slate-200 rounded-lg">
       <h1 className="text-3xl text-brand">Keep It - Keep the file stored</h1>
     </div>
   );
